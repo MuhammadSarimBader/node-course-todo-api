626cfb44f67ff3133345f70c1600fbed67bcfb6b
Apple <apple@Apples-MacBook-Pro-3.local>
2019-01-22 20:18:36 +0500

    Add POST /todos route and refactor

---
+++ server/server.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {User} = require('./models/user');
+var {Todo} = require('./models/todo');
+
+var app = express();
+
+
+//Create Read Update Delete CRUD.
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
+
+
+
+// var newTodo = new Todo({
+//   text: 'Cook dinner'
+// });
+//
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo',doc);
+// }, (e) => {
+//   console.log('Unable to save todo');
+// });
+
+
+// var otherTodo = new Todo({
+//   text: 'Feed the cat',
+//   completed: true,
+//   completedAt: 123
+// });
+//
+// otherTodo.save().then((doc) => {
+//   console.log('Saved todo',doc);
+// }, (e) => {
+//   console.log('Unable to save todo');
+// }
+
+
+// var user = new User({
+//   email: 'sarim.bader@hotmail.com  '
+// });
+//
+// user.save().then((doc) => {
+//   console.log('user saved', doc);
+// }, (e) => {
+//   console.log('Unable to save user', e);
+// });
